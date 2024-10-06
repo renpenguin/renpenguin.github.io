f585b6a48870720d5a0d992c2711085c268bc591
Red Penguin <redpenguin777@yahoo.com>
2024-10-06 18:04:55 +0100

    added a flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "Flake for Github Pages + Jekyll development";
+
+	inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+	  let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ bundler jekyll rubyPackages.github-pages ];
+      };
+    };
+}
